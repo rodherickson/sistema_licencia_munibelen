f0e711ef608c9abf44c93df96675b74cc2c96082
Journii <ginopaflo001608@gmail.com>
2024-01-26 12:14:13 -0500

    Modificacion de json para error de authenticate

--- app/Http/Requests/CarnetRequest.php
+++ app/Http/Requests/CarnetRequest.php
@@ -6,7 +6,6 @@ use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Validation\Rule;
 
 class CarnetRequest extends FormRequest
 {
